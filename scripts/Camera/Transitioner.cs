e04af3fd117ea9c75692244c9729a4a11f4cac1d
TheArchitect <remi@nosoft.io>
2025-03-20 16:13:11 +0100

    CLR Rename Transit in Trig

--- scripts/Camera/Transitioner.cs
+++ scripts/Camera/Transitioner.cs
@@ -1,10 +1,12 @@
+using System.Collections;
+
 using UnityEngine;
 
 public class Transitioner : Transitioner<Transition> { }
 public class Transitioner<T> : MonoBehaviour where T : Transition {
 	public T transition;
 
-	public void Transit(){
+	public void Trig(){
 		if(transition || TryGetComponent( out transition )) transition.Trig();
 	}
 }
